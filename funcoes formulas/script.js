981fe4bcaedf736f53a58c59a1bd6e90001ef031
Brian de Souza <briandesouza1597@gmail.com>
2024-07-31 10:31:39 -0300

    commit calculo perimetro e area atualizado

--- funcoes formulas/script.js
+++ funcoes formulas/script.js
@@ -3,6 +3,12 @@ function calcularPerimetroTriangulo (ladoA, ladoB, ladoC) {
     return perimetro
 }
 
+function calcularAreaTriangulo(ladoA, ladoB, ladoC) {
+    var s = (ladoA + ladoB + ladoC) / 2;
+    var area = Math.sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC));
+    return area;
+  }
+
 function processarFormulario(event) {
     event.preventDefault()
 
@@ -10,12 +16,23 @@ function processarFormulario(event) {
     var ladoB = parseFloat(document.getElementById('ladob').value)
     var ladoC = parseFloat(document.getElementById('ladoc').value)
 
-    var resultadoCalculo = calcularPerimetroTriangulo(ladoA, ladoB, ladoC)
+    if (isNaN(ladoA) || isNaN(ladoB) || isNaN(ladoC)) {
+        document.getElementById('resultado').innerText = "Por favor, insira valores válidos para todos os lados.";
+        return;
 
-    document.getElementById('resultado').innerText = "O resultado eh: " + resultadoCalculo.toString().replace(".", ",")
 
+      }
+      var perimetro = calcularPerimetroTriangulo(ladoA, ladoB, ladoC);
+  var area = calcularAreaTriangulo(ladoA, ladoB, ladoC);
+
+  document.getElementById('resultado').innerText = 
+    "O perímetro é: " + perimetro.toFixed(2).replace(".", ",") + 
+    " e a área é: " + area.toFixed(2).replace(".", ",");
 }
 
 document
 .getElementById('form-perimetro-triangulo')
 .addEventListener('submit', processarFormulario)
+document
+//.getElementById('calcular-area')
+//.addEventListener('click', processarFormulario);
